a6480e84b51eda1e64d3df6ad8895e704db7fc0d
Matheus Bignotto <matheus.bignottodev@gmail.com>
2020-11-01 10:44:04 -0300

    Conceito de DTO 😎

--- back-end/src/routes/appointments.routes.ts
+++ back-end/src/routes/appointments.routes.ts
@@ -27,7 +27,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'Esse horário já esta agendado' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json({ appointment });
 });
