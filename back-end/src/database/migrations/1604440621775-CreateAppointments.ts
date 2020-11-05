38566b7f09973345586e6b6ecdf6554f09be5104
Matheus Bignotto <matheus.bignottodev@gmail.com>
2020-11-05 16:51:13 -0300

    mudanca para mudar o postgress ✔

--- back-end/src/database/migrations/1604440621775-CreateAppointments.ts
+++ back-end/src/database/migrations/1604440621775-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1604440621775
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
